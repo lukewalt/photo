188f35bfd2346d6d1e06f5b44e00de23770f67ee
Luke Woodward <luke.wdwrd@gmail.com>
2017-08-03 09:42:31 -0500

    finished creating store

--- client/store.js
+++ client/store.js
@@ -4,3 +4,13 @@ import { browserHistory } from 'react-redux';
 import rootReducer from './reducers/index';
 import comments from './data/comments';
 import posts from './data/posts';
+
+
+const defaultState = {
+  posts,
+  comments
+};
+
+// creates store using interface of the rootReducer with data of init state
+export default const store = createStore(rootReducer, defaultState);
+export const history = syncHistoryWithStore(browserHistory)
