384a90ef276d01ee75a599826eaa1904c5d52bf4
Luke Woodward <luke.wdwrd@gmail.com>
2017-08-05 18:28:38 -0500

    created like and comment view on UI from state data

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
 
 export default class PhotoGrid extends Component {
 
@@ -6,9 +7,7 @@ export default class PhotoGrid extends Component {
   render() {
     return (
       <div className="photo-grid">
-        <pre>
-          {JSON.stringify(this.props.posts, null, ' ')}
-        </pre>
+        {this.props.posts.map((post, i) => <Photo {... this.props} key={i} i={i} post={post}/>)}
       </div>
     )
   }
