a7090981969a55ae4f8b393287c00ca8d1052f0f
Luke Woodward <luke.wdwrd@gmail.com>
2017-08-08 10:01:31 -0500

    created logic to update state based on which action is being dispatched

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -2,16 +2,39 @@
 // the action (info about what happened)
 // copy of the current state
 
+
+// ONLY CONCERNED WITH WHOLE STATE OF COMMENTS SECTION
 function comments(state = [], action) {
-  console.log('adding comments');
-  switch(action.type){
-    case 'ADD_COMMENT' :
-      
+  if (typeof action.postId !== 'undefined') {
+    return {
+      //take current state
+      ...state,
+      // overwrite this posts comment section with a new one
+      // all this is concerned with is the state of comments
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  return state;
+
+}
+
+
+function postComments(state = [], action) {
+  // discerns which action is being called and executes state change
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      // return existing state plus new comment
+      return [...state,{
+        user: action.author,
+        text: action.comment
+      }]
+    case 'REMOVE_COMMENT':
+      return state
     default:
       return state
   }
 
-  return state;
+  return state
 }
 
 export default comments;
