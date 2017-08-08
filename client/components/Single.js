1267bdac931472c354258a9e780fe5157b1af1b6
Luke Woodward <luke.wdwrd@gmail.com>
2017-08-08 09:42:13 -0500

    displayed comments in single page view; created form to comment

--- client/components/Single.js
+++ client/components/Single.js
@@ -6,14 +6,19 @@ export default class Single extends Component {
 
 
   render() {
+    // postId pulled from params
+    const { postId } = this.props.params
     //index of post
-    const i = this.props.posts.findIndex( posts => posts.code === this.props.params.postId);
+    const i = this.props.posts.findIndex( posts => posts.code === postId);
     // get the post
     const post = this.props.posts[i];
+    // get comments for post ; allows looping over no comments and push one for the first time
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props}/>
-        <Comments />
+        <Comments i={i} postComments={postComments} {...this.props}/>
       </div>
     )
   }
